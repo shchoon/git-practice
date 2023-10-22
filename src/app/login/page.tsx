50f2274710a4dd2d87468d3fcf8193185d651e5c
shcho <acckr0604@gmail.com>
2023-10-22 16:50:26 +0900

    login UI 구현

---
+++ src/app/login/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+import '../globals.css'
+import Image from 'next/image'
+
+export default function Login() {
+    return(
+        <div className='flex justify-center h-[1080px] items-center py-[272px] w-[1920px]  bg-[#F8FAFD]'>
+        <div className='flex items-start w-[442px]  h-[476px] gap-2.5 px-[6px] pt-3 pb-2 border rounded-2xl border-[#E5E7EB] bg-[#FFF] shadow-[-3px_5px_20px_0px_rgba(0,0,0,0.03)]'>
+            <div className='flex flex-col  items-center gap-[56px]'>
+                <div className=' flex items-center flex-col w-[383px] h-[150px] gap-[30px]'>
+                    <div className='w-[60px] h-[60px] p-[5px]'>
+                        <img src='/logo.png' alt='' />
+                    </div>
+                    <div className='flex flex-col w-[383px] h-[60px] items-center gap-2'>
+                        <div className='text-center text-[28px] font-bold leading-normal'>센스있는 매니csc저, 오센스</div>
+                        <div className='text-center text-[13px] font-normal leading-normal text-[#4B5563]'>오감을 깨우는 클래스 관리, 센스 만점 오센스에서 시작하세요!</div>
+                        {/* font-size 다름 */}
+                    </div>
+                </div>
+                <div className='w-[430px] h-[250px] flex flex-col items-start gap-2'>
+                    <div className='flex w-[430px] h-[52px] rounded-md bg-[#FFE812]'>
+                        <svg xmlns="http://www.w3.org/2000/svg" className='my-0.5 ml-2' width="48" height="48" viewBox="0 0 48 48" fill="none">
+                        <path d="M23.9997 14.6934C18.43 14.6934 13.915 18.2534 13.915 22.6448C13.915 25.4838 15.8026 27.9751 18.642 29.3818C18.4875 29.9145 17.6494 32.809 17.616 33.0364C17.616 33.0364 17.5959 33.2072 17.7065 33.2724C17.8172 33.3375 17.9473 33.2869 17.9473 33.2869C18.2646 33.2426 21.6266 30.8811 22.2085 30.471C22.7898 30.5533 23.3884 30.5961 23.9997 30.5961C29.5693 30.5961 34.0843 27.0362 34.0843 22.6448C34.0843 18.2534 29.5693 14.6934 23.9997 14.6934Z" fill="#381F1F"/>
+                        </svg>
+                        <div className='font-semibold w-[190px] h-[16px] leading-4 mt-[18px] mx-auto'>카카오 계정으로 회원가입</div>
+                    </div>
+                    <div className='flex w-[430px] h-[52px] rounded-md border border-[#2BB500]'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+                        <path d="M15.6729 16.6528V31.3467H21.4486V23.5498L26.9354 31.3467H32.3259V16.6528H26.9354V24.6493L21.4486 16.6528H15.6729Z" fill="#2BB500"/>
+                        </svg>
+                        <div className='mx-auto font-semibold h-[16px] leading-4 mt-[18px] text-[#2BB500]'>네이버 계정으로 회원가입</div>
+                    </div>
+                    <div className='flex w-[430px] h-[52px] rounded-md border border-[#6B7280]'>
+                        <svg className='p-[15px]' xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 18 18" fill="none">
+                        <g clip-path="url(#clip0_1045_4962)">
+                            <path d="M17.8242 9.20731C17.8242 8.59552 17.7746 7.98041 17.6688 7.37854H9.17969V10.8443H14.041C13.8393 11.962 13.1911 12.9508 12.242 13.5792V15.8279H15.1422C16.8453 14.2604 17.8242 11.9455 17.8242 9.20731Z" fill="#4285F4"/>
+                            <path d="M9.18032 18.0006C11.6077 18.0006 13.6547 17.2036 15.1461 15.8279L12.2459 13.5792C11.439 14.1281 10.3973 14.439 9.18363 14.439C6.83566 14.439 4.84484 12.8549 4.13053 10.7252H1.1377V13.0434C2.66553 16.0826 5.77742 18.0006 9.18032 18.0006Z" fill="#34A853"/>
+                            <path d="M4.12659 10.7252C3.74959 9.60745 3.74959 8.39709 4.12659 7.27933V4.96112H1.13706C-0.139438 7.5042 -0.139438 10.5003 1.13706 13.0434L4.12659 10.7252Z" fill="#FBBC04"/>
+                            <path d="M9.18032 3.56224C10.4634 3.5424 11.7036 4.02523 12.6328 4.9115L15.2024 2.34196C13.5753 0.814129 11.4159 -0.0258495 9.18032 0.000606499C5.77742 0.000606499 2.66553 1.91867 1.1377 4.96111L4.12722 7.27931C4.83823 5.1463 6.83235 3.56224 9.18032 3.56224Z" fill="#EA4335"/>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_1045_4962">
+                            <rect width="18" height="18" fill="white"/>
+                            </clipPath>
+                        </defs>
+                        </svg>
+                        <div className='mx-auto font-semibold h-[16px] leading-4 mt-[18px]'>구글 계정으로 회원가입</div>
+                    </div>
+                    <div className='w-[430px] h-[52px] rounded-md border border-[#6B7280]'></div>
+                </div>
+            </div>
+        </div>
+        </div>
+        
+    )
+}
+
